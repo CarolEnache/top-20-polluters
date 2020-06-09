8fd77fc7a2ac97df4cb861f97d60f2340c9eea8f
CarolEnache <enache.carol@gmail.com>
2020-06-09 15:44:42 +0100

    Improve the Button

--- src/components/modal/Button.tsx
+++ src/components/modal/Button.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Button, CompanyName } from './styled';
 
+interface Polluter {
+  Name: string;
+}
 interface ModalButtonProps {
   keys: string[];
-  polluter: string;
-  onClick: () => {};
+  polluter: Polluter;
+  onClick: (f: number) => {};
 }
 
 const ModalButton = ({ keys = [], polluter, onClick }: ModalButtonProps) => {
-  return keys.map((key: any) => {
+  return keys.map((key: string) => {
     if (key === 'Name') {
       const separate = polluter[key].split(',');
       return (

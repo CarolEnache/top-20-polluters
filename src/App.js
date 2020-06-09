525a50a4484566821871aa546154aafe50743639
CarolEnache <enache.carol@gmail.com>
2020-06-09 14:38:20 +0100

    rename the directory for custom hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useFetch } from './custom-hooks';
+import { useFetch } from './hooks';
 
 import { List } from './components/list';
 
